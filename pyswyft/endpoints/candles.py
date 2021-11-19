d3257a70016c0d1719c413fd9f01e3161f9a4a3d
Lachlan Teale <lachlan.teale@gmail.com>
2021-11-19 22:28:07 +1000

    FEAT: Added Candles Endpoints
    Currently undocumented in Swyftx API docs but can be pulled from their charting (Tradingview)

---
+++ pyswyft/endpoints/candles.py
@@ -0,0 +1,40 @@
+""" Handle Candles endpoints"""
+from .apirequest import APIRequest
+from .decorators import endpoint
+from abc import abstractmethod
+from datetime import datetime
+
+
+class Candles(APIRequest):
+    """Candles - class to handle the Candles endpoints"""
+
+    @abstractmethod
+    def __init__(self):
+        endpoint = self.ENDPOINT.format()
+        super(Candles, self).__init__(endpoint, method=self.METHOD)
+
+
+@endpoint('charts/getBars/', 'GET')
+class CandlesGetBars(Candles):
+    """CandlesGetBars - class to handle the CandlesGetBars endpoint"""
+    def __init__(self, primary, secondary, side, resolution, timeEnd, timeStart):
+        super(CandlesGetBars, self).__init__()
+
+        converted_timeEnd = int((timeEnd - datetime.utcfromtimestamp(0)).total_seconds() * 1000.0)         # convert to milliseconds since epoch
+        converted_timeStart = int((timeStart - datetime.utcfromtimestamp(0)).total_seconds() * 1000.0)     # convert to milliseconds since epoch
+
+        self.ENDPOINT = self.ENDPOINT + str(primary) + '/' + str(secondary) + '/' + str(side) + '/'
+        self.params = {'resolution': resolution, 'timeEnd': converted_timeEnd, 'timeStart': converted_timeStart}
+
+
+@endpoint('charts/getLatestBar/', 'GET')
+class CandlesGetLatestBar(Candles):
+    """CandlesGetLatestBar - class to handle the CandlesGetLatestBar endpoint"""
+    def __init__(self, primary, secondary, side, resolution):
+        super(CandlesGetLatestBar, self).__init__()
+
+        self.ENDPOINT = self.ENDPOINT + str(primary) + '/' + str(secondary) + '/' + str(side) + '/'
+        self.params = {'resolution': resolution}
+
+# https://api.swyftx.com.au/charts/getBars/AUD/ETH/ask/?resolution=4h&timeEnd=1637058005000&timeStart=1616321945000 - GET
+# https://api.swyftx.com.au/charts/getLatestBar/AUD/ETH/ask/?resolution=4h - GET
